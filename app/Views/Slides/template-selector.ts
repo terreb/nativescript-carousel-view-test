55df0ed6d1b43104aa04434d31506efa5cecdffb
Alex Mantsurov <user@users-MacBook-Pro.local>
2016-11-24 12:44:02 +0200

    updated plugin version

--- app/Views/Slides/template-selector.ts
+++ app/Views/Slides/template-selector.ts
@@ -1,13 +1,17 @@
 import { ITemplateSelector } from "nativescript-carousel-view";
+import { topmost } from 'ui/frame'
 import builder = require("ui/builder");
 
 export class MyTemplateSelector implements ITemplateSelector {
     
     OnSelectTemplate(position: number, bindingContext: any) {
 
+        const page = topmost().currentPage
+
         var view = builder.load({
             path: "~/Views/Slides",
-            name: "slider-view"
+            name: "slider-view",
+            //page: page
         });
 
         view.bindingContext = bindingContext;
